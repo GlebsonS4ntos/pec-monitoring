a98793df249d1d4dcf02ddd6efe4a050fd46f76e
Glebson Santos <glebonsantos@outlook.pt>
2025-04-18 00:55:52 -0300

    feat: changing file with service data

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ import os
 def mudar_status(servico, novoStatus):
     linhas = []
 
-    with open('pec_urls.csv', newline='') as csvfile:
+    with open('service_urls.csv', newline='') as csvfile:
         reader = csv.reader(csvfile, delimiter=';')
         for linha in reader:
             if linha[0] == servico:
                 linha[2] = novoStatus
             linhas.append(linha)
 
-    with open('pec_urls.csv', 'w', newline='') as csvfile:
+    with open('service_urls.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=';')
         writer.writerows(linhas)
 
@@ -39,7 +39,7 @@ async def fazer_requisicao(session, url, servico, statusMunicipio):
 
 
 async def main():
-    with open('pec_urls.csv') as csvfile:
+    with open('service_urls.csv') as csvfile:
         file = csv.reader(csvfile, delimiter=';')
         async with aiohttp.ClientSession() as session:
             tarefas = [
