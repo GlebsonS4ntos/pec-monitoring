bfad989beba15a23890d3841d114488267e77953
Glebson Santos <glebonsantos@outlook.pt>
2025-04-18 23:08:06 -0300

    feat: add /help and /status

--- send.py
+++ send.py
@@ -2,6 +2,7 @@ import telegram
 from telegram import ForceReply, Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 import os
+import csv
 
 def armazenar_usuario(usuarioId):
     if not os.path.exists("usuarios.txt"):
@@ -46,13 +47,34 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     armazenar_usuario(user.id)
 
     await update.message.reply_html(
-        rf"Olá {user.mention_html()}, seja bem-vindo! Aqui você será notificado sempre que algum serviço da Marques Consult apresentar instabilidade ou ficar fora do ar, além de ser avisado quando os serviços forem normalizados.",
-        reply_markup=ForceReply(selective=True),
+    rf"Olá {user.mention_html()}, seja bem-vindo! Aqui você será notificado sempre que algum serviço da {os.getenv('ENTERPRISE')} apresentar instabilidade ou ficar fora do ar, além de ser avisado quando os serviços forem normalizados. Use /help para ver os comandos disponíveis."
     )
+
+async def status(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    message = ""
+
+    with open('service_urls.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=';')
+        for linha in reader:
+            if linha[2] == "false":
+                message += f"{linha[0]} - Offline ❌ \n"
+            else:
+                message += f"{linha[0]} - Online ✅ \n"
+            
+    
+    await update.message.reply_html(
+        message,
+    )
+
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text("Use /status para se informar sobre o status atual dos serviços. Obs: a última verificação ocorreu, no máximo, há 10 segundos.")
+
 
 async def run_bot():
     application = Application.builder().token(os.getenv("TELEGRAM_TOKEN")).build()
     application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("status", status))
+    application.add_handler(CommandHandler("help", help))
 
     await application.initialize()
     await application.start()
